1f43489ec7fd503e97c82ff93dbec595d1d05863
Yannissss <30572882+Yannissss@users.noreply.github.com>
2024-01-28 19:22:33 +0100

    Update vars.sh

--- telemac/vars.sh
+++ telemac/vars.sh
@@ -1,15 +1,6 @@
 # Top-level dirs
 export BUILDDIR=$(pwd)
 
-# Load environnement
-#source env.sh
-
-# Build variables
-export CC=gcc
-export CXX=g++
-export FC=gfortran
-export N=8
-
 export METISDIR=$BUILDDIR/METIS
 export HDF5DIR=$BUILDDIR/HDF5
 export MEDDIR=$BUILDDIR/MED
